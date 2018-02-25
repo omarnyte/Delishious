fd4071f5691481c99709ba91f5bc20e505ff3c5b
Omar <omar.dlsg@gmail.com>
2018-02-25 13:35:57 -0800

    Direct / and /stores GET routes to getStores controller

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -10,8 +10,15 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store'});
 };
 
-exports.createStore = (req, res) => {
-  const store = new Store(req.body);
-  // await store.save();
-  res.redirect('/');
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save();
+  await store.save();
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = (req, res) => {
+  res.render('stores', {title: 'Stores'});
+}
+
+ 
