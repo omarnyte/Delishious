37c9886229501738835376c641883d01a083924d
Omar <omar.dlsg@gmail.com>
2018-02-26 15:31:39 -0800

    Remove console logging

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index');
 };
 
@@ -19,7 +18,6 @@ exports.createStore = async (req, res) => {
 
 exports.getStores = async (req, res) => {
   const stores = await Store.find();
-  console.log(stores);
   res.render('stores', {title: 'Stores', stores});
 }
 
