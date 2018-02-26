c3cf9e37c5944f0b3f8506afced10a2cf7135fa1
Omar <omar.dlsg@gmail.com>
2018-02-26 13:46:20 -0800

    Create POST /add/:id route to edit existing stores. Create updateStore controller method

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -23,4 +23,17 @@ exports.getStores = async (req, res) => {
   res.render('stores', {title: 'Stores', stores});
 }
 
- 
+ exports.editStore = async (req, res) => {
+   const store = await Store.findOne({_id: req.params.id});
+
+   res.render('editStore', { title: `Edit ${store.name}`, store} );
+ }
+
+ exports.updateStore = async (req, res) => {
+   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+     new: true, // returns the new store instead of the old one 
+     runValidators: true
+   }).exec();
+   req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store =></a>`)
+   res.redirect(`/stores/${store._id}/edit`);
+ }
