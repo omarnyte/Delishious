705f90b874a1c1c07dc75596c9c9e713f65babf8
Omar <omar.dlsg@gmail.com>
2018-02-27 17:30:47 -0800

    Create register layout

--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,7 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
 
 module.exports = router;
 
