0df569f26c85541821850cea021e996a02737bc2
Omar <omar.dlsg@gmail.com>
2018-02-28 18:57:10 -0800

    Validate registration

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,10 @@ router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
 router.get('/register', userController.registerForm);
+router.post('/register',
+    userController.validateRegister,
+    catchErrors(userController.register)
+);
 
 module.exports = router;
 
