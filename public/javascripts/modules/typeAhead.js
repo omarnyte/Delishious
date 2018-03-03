c9b2c1f5f4e9b19860ba9bca8c268b33aa0429d4
Omar <omar.dlsg@gmail.com>
2018-03-03 10:48:22 -0800

    Create typeAhead.js module with typeAhead function using axios

---
+++ public/javascripts/modules/typeAhead.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+
+function searchResultsHTML (stores) {
+    return stores.map(store => {
+        return `
+            <a href="/stores/${store.slug}" class="search__result"> 
+                <strong>${store.name}</strong>
+            </a>    
+        `;
+    }).join('');
+}
+
+function typeAhead (search) {
+    if (!search) return;
+    
+    const searchInput = search.querySelector('input[name="search"]');
+    const searchResults = search.querySelector('.search__results');
+
+    searchInput.on('input', function() {
+        if(!this.value) {
+            searchResults.style.display = 'none';
+            return;
+        }    
+        
+        searchResults.style.display = 'block'; 
+        searchResults.innerHTML = '';
+
+        axios
+            .get(`/api/search?q=${this.value}`)
+            .then(res => {
+                if (res.data.length) {
+                    searchResults.innerHTML = searchResultsHTML(res.data);;
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    })    
+}
+
+export default typeAhead;
